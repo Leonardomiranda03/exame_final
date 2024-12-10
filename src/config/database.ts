4a2ab687716561f54036cb956185052cf225b93c
Leonardomiranda03 <leorodrigomiranda13@gmail.com>
2024-12-10 20:06:01 -0300

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,1 +1,13 @@
+import { Sequelize } from 'sequelize';
+
+// A URL de conexão pode ser obtida na plataforma de banco de dados (Railway, Render, etc.)
+const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://exame_final_user:o5qdJejFjYtYVdLiUInWWg0ENZRCXqZb@dpg-ctcc4t3tq21c73fo41kg-a.oregon-postgres.render.com/exame_final';
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false, // Desabilita os logs SQL (opcional)
+});
+
+export default sequelize;
 
