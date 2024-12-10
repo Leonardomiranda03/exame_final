f4db84fcf9eceb798bbf73c2f4a7776c3329571c
Leonardomiranda03 <leorodrigomiranda13@gmail.com>
2024-12-10 20:08:38 -0300

    Update Prancha.ts

--- src/models/Prancha.ts
+++ src/models/Prancha.ts
@@ -1,1 +1,40 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database';
+
+class Prancha extends Model {
+  public id!: number;
+  public quantidade!: number;
+  public tamanhoEmPes!: number;
+  public cor!: string;
+}
+
+Prancha.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    quantidade: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    tamanhoEmPes: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    cor: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Prancha',
+    tableName: 'pranchas',
+    timestamps: false,
+  }
+);
+
+export default Prancha;
 
