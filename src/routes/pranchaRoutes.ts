27d8e0701576ff7fd8ca1c919256976a24d3671e
Leonardomiranda03 <leorodrigomiranda13@gmail.com>
2024-12-10 20:11:03 -0300

    Update pranchaRoutes.ts

--- src/routes/pranchaRoutes.ts
+++ src/routes/pranchaRoutes.ts
@@ -1,1 +1,10 @@
+import { Router } from 'express';
+import PranchaController from '../controllers/PranchaController';
+
+const router = Router();
+
+router.get('/pranchas', PranchaController.getAll);
+router.post('/pranchas', PranchaController.create);
+
+export default router;
 
