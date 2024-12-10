a959c032dd5ec2f3cf9e1f5591170971ab18db3f
Leonardomiranda03 <leorodrigomiranda13@gmail.com>
2024-12-10 20:40:14 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import express from 'express';
+import pranchaRoutes from './routes/pranchaRoutes.';
+import sequelize from './config/database';
+
+const app = express();
+app.use(express.json());
+
+app.use(pranchaRoutes);
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Conectado ao banco de dados'))
+  .catch((error) => console.error('Erro ao conectar ao banco de dados', error));
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
 
