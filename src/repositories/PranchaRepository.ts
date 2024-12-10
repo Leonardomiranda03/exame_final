7f847c5ca3e0bd607c163402f72971cb0576f61c
Leonardomiranda03 <leorodrigomiranda13@gmail.com>
2024-12-10 20:10:01 -0300

    Update PranchaRepository.ts

--- src/repositories/PranchaRepository.ts
+++ src/repositories/PranchaRepository.ts
@@ -1,1 +1,14 @@
+import Prancha from '../models/Prancha';
+
+class PranchaRepository {
+  async create(pranchaData: { quantidade: number; tamanhoEmPes: number; cor: string }) {
+    return Prancha.create(pranchaData);
+  }
+
+  async findAll() {
+    return Prancha.findAll();
+  }
+}
+
+export default new PranchaRepository();
 
